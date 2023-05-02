7eb1104bfe8eef7a5c4929bd0b2a4ba37f6d0cc4
Egzon Arifaj <egzonarifaj@Egzons-MBP.fritz.box>
2023-05-02 18:23:21 +0200

    Update login form inputs with useRef to hide the value of inputs from being visible on DOM

--- src/features/Login.tsx
+++ src/features/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef } from "react";
 import { NavLink } from "react-router-dom";
 import { MButton } from "../components/Buttons";
 import useAuth from "../hooks/useAuth";
@@ -6,17 +6,28 @@ import useAuth from "../hooks/useAuth";
 const Login = () => {
 	const { onLogin } = useAuth();
 
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+	const emailInput = useRef<HTMLInputElement>(null);
+	const passwordInput = useRef<HTMLInputElement>(null);
 
-	const handleEmailOnChange = (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value);
-	const handlePasswordOnChange = (e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value);
+	const handleEmailOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		if(emailInput.current) emailInput.current.value = e.target.value;
+	}
+	const handlePasswordOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		if(passwordInput.current) passwordInput.current.value = e.target.value;
+	}
 
 	const handleOnSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 
 		try {
-			onLogin({ email, password });
+			if(!emailInput.current || !passwordInput.current) {
+				return;
+			}
+
+			onLogin({ 
+				email: emailInput.current.value, 
+				password: passwordInput.current.value 
+			});
 		}
 		catch (err) {
 			console.log(err);
@@ -28,8 +39,8 @@ const Login = () => {
 			<h1 className="text-4xl mb-5">Login</h1>
 			<form onSubmit={handleOnSubmit} className="flex flex-col gap-6 items-center">
 				<div className="flex flex-col gap-2">
-					<input type="email" autoFocus placeholder="Email" value={email} onChange={handleEmailOnChange} className="py-1 px-3 rounded-md font-extralight bg-slate-200" required />
-					<input type="password" placeholder="Password" value={password} onChange={handlePasswordOnChange} className="py-1 px-3 rounded-md font-extralight bg-slate-200" required />
+					<input ref={emailInput} type="email" autoFocus placeholder="Email" onChange={handleEmailOnChange} className="py-1 px-3 rounded-md font-extralight bg-slate-200" required />
+					<input ref={passwordInput} type="password" placeholder="Password" onChange={handlePasswordOnChange} className="py-1 px-3 rounded-md font-extralight bg-slate-200" required />
 					<p className="text-xs inline-flex pl-1">
 						Don't have an account?&nbsp;
 						<NavLink to="/register" className="inline-flex items-center font-bold text-blue-600 dark:text-blue-500 hover:underline">
